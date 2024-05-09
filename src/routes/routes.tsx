d2aabaff10dcb589a11d0604653ac70e83c86e8a
Axel Husson <aaaxel.hsson@gmail.com>
2024-05-09 16:32:16 +0200

    [ROUTING] Add routes to the project

---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { AboutPage } from "../pages/AboutPage";
+import { ErrorPage } from "../pages/ErrorPage";
+import { FicheLogementPage } from "../pages/FicheLogementPage";
+import { HomePage } from "../pages/HomePage";
+
+import { Route, Routes, Navigate } from "react-router-dom";
+
+export const RouteList = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/house/:id" element={<FicheLogementPage />} />
+      <Route path="/404" element={<ErrorPage />} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
+};
+
+
