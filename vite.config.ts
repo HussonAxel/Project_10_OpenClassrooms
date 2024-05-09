840bc9f2e9505418147a56ce7cc03ce1e399438b
Axel Husson <aaaxel.hsson@gmail.com>
2024-05-09 16:30:52 +0200

    [CHORE] Add aliases to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from "sass";
+import { resolve } from "path";
 
+const root = resolve(__dirname, "src");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +12,16 @@ export default defineConfig({
       scss: {
         implementation: sass,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@components": resolve(root, "components"),
+      "@pages": resolve(root, "pages"),
+      "@routes": resolve(root, "routes"),
+      "@sass": resolve(root, "sass"),
+      "@utils": resolve(root, "utils"),
+      "@data": resolve(root, "data"),
     },
   },
 });
